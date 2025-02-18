1fe8d5ac0aaf1e6fecf6fcafa99079f53f133f73
Waqar Waheed <gtawaqar222@gmail.com>
2025-02-19 00:20:45 +0500

    setup the rest controller/logger/spring-boot3 (#2)
    * Setup rest API, Logger configuration, spring boot 3.0 migration.
    
    ---------
    Co-authored-by: Adilwaheed<adilwaheed474@gmail.com>

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -6,6 +6,7 @@ sudo yum -y install java-17-amazon-corretto-headless
 # Directory paths
 DIR1="/opt/bwg-service"
 DIR2="/opt/bwg-service/config"
+DIR3="/opt/bwg-service/logs"
 
 # Check and create the first directory
 if [ -d "$DIR1" ]; then
@@ -21,4 +22,12 @@ if [ -d "$DIR2" ]; then
 else
   sudo mkdir -p "$DIR2"
   echo "Directory $DIR2 created."
+fi
+
+# Check and create the second directory
+if [ -d "$DIR3" ]; then
+  echo "Directory $DIR3 already exists."
+else
+  sudo mkdir -p "$DIR3"
+  echo "Directory $DIR3 created."
 fi
