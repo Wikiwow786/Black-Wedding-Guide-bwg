1c18305d26995786f892b2792d08f6434250ea62
Waqar Waheed <gtawaqar222@gmail.com>
2025-02-19 08:24:08 +0100

    remove log4J

--- src/main/java/com/bwg/restapi/HelloWorldController.java
+++ src/main/java/com/bwg/restapi/HelloWorldController.java
@@ -1,18 +1,15 @@
 package com.bwg.restapi;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
-@Log4j2
 public class HelloWorldController {
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> hello() {
-        log.info("Inside Hello World Controller!!!");
         return ResponseEntity.ok("Hello BWG World!");
     }
 }
