edc6bcdd1093e82c81073001bcfdb5c2bb39268e
Waqar Waheed <gtawaqar222@gmail.com>
2025-04-01 10:31:24 +0200

    remove auth model in review API

--- src/main/java/com/bwg/restapi/ReviewsController.java
+++ src/main/java/com/bwg/restapi/ReviewsController.java
@@ -26,7 +26,9 @@ public class ReviewsController {
 
     @PermitAll
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Page<ReviewsModel>> getAllReviews(@RequestParam(required = false) Integer rating, @RequestParam(required = false) List<Long> serviceId, Pageable pageable, @AuthPrincipal AuthModel authModel) {
+    public ResponseEntity<Page<ReviewsModel>> getAllReviews(@RequestParam(required = false) Integer rating,
+                                                            @RequestParam(required = false) List<Long> serviceId,
+                                                            Pageable pageable) {
         return ResponseEntity.ok(reviewsService.getAllReviews(rating, serviceId, pageable).map(ReviewsModel::new));
     }
 
